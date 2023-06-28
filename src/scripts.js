d580e5c73a95caf530b16bd0f491c9add3fd009e
workmanmcr <workmanmcr@gmail.com>
2023-06-28 14:39:15 -0700

    add scroll console log

--- src/scripts.js
+++ src/scripts.js
@@ -8,18 +8,31 @@ const count = 10;
 const apiKey = 'A9PGyt_01t7OB9UMaBVs9dVOo7j88ERdApNub9WMzcg';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// helper function 
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 function displayPhotos() {
   // run function for each object 
   photosArray.forEach((photo) => {
 // create <a> to link to unsplash
     const item = document.createElement('a')
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+   
+    setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank'
+    });
     // creat <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', photo.url.regular);
-    img.setAttribute('alt', photo.alt.description);
-    img.setAttribute('title', photo.alt.description);
+ 
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    });
   // put img inside anchor element then put both inside image-container 
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -35,5 +48,8 @@ async function getPhotos() {
   } catch (error) {
   }
 }
-
+// check to see if scrolling near bottom of page, load more pics
+window.addEventListener('scroll', () => {
+  console.log('scrolled');
+})
 getPhotos();
